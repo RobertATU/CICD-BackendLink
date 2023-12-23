5833246d2382a6748f51e6c17559477e5247ead9
Robert <g00395612@atu.ie>
2023-12-23 13:42:10 +0000

    Initial Commit

---
+++ src/main/java/com/example/cicdbackendlink/CicdBackendLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdbackendlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdBackendLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdBackendLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdbackendlink/CicdBackendLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdbackendlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdBackendLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
